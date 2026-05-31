0e06ce88a23d0262290a17d543640e8c0da23313
Hasnain-KH <webdevhasnain@gmail.com>
2026-05-31 12:06:45 +0500

    add some of the tasks

---
+++ Task from teacher/app.ts
@@ -0,0 +1,29 @@
+// let username: string = "hasnain";
+// username = "Sufyan";
+// console.log(username);
+
+
+// let age: number = 17;
+// age = 18;
+// console.log(age);
+
+// let inrolled: boolean = false;
+// inrolled = true;
+// console.log(inrolled);
+
+// let coursefee: number = 1000;
+// coursefee = 1035;
+// console.log(coursefee);
+
+
+
+// let multi = (a: number, b:number):number =>{
+//     return a * b;
+// }
+
+// console.log(multi(5, 5));
+
+let sub = (a:number, b:number): number => {
+    return a - b;
+}
+console.log(sub(10, 5));
