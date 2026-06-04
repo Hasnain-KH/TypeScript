68f5dd69a8abb73ee2824dec47285ed1bce4bf57
https://github.com/Hasnain-KH <webdevhasnain@gmail.com>
2026-06-05 04:02:55 +0500

    add experimental TypeScript decorators code

--- Decorators/app.ts
+++ Decorators/app.ts
@@ -6,30 +6,16 @@
 
 // class student{}
 
-// function logger1 (constructor: Function){
-//     console.log("OH, It's running");
-// }
-// @logger1
-
-// class person{}
-// function load(constructor : Function){
-//     console.log("Student class loaded");
-// }
-
-// @load
-
-// class student{}
-
-
-function logMethod(target: any,propertyName: string, descriptor: PropertyDescriptor) {
-  console.log("Method is being called:", propertyName);
+function logger1 (constructor: Function){
+    console.log("OH, It's running");
 }
+@logger1
+class person{}
 
-class Student {
-  @logMethod
-  sayHello() {
-    console.log("Hello!");
-  }
+
+function load(constructor : Function){
+    console.log("Student class loaded");
 }
-
+@load
+class student{}
 
