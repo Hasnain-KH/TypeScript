9b455f14887a39e57c6705555a4f49251d9b313e
https://github.com/Hasnain-KH <webdevhasnain@gmail.com>
2026-06-04 23:57:13 +0500

    add Two simple tasks of Decorator

---
+++ Decorators/app.ts
@@ -0,0 +1,14 @@
+// function logger(constructor: Function){
+//     console.log("Decorator chal gaya !");
+// }
+
+// @logger
+
+// class student{}
+
+// function logger1 (constructor: Function){
+//     console.log("OH, It's running");
+// }
+// @logger1
+
+// class person{}
