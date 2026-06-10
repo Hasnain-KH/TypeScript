2ee1a43b2e11c93b39fea938e3988ae84df5f8a5
https://github.com/Hasnain-KH <webdevhasnain@gmail.com>
2026-06-10 23:47:10 +0500

    Add some of the practic of type aliases

---
+++ type-aliases/app.js
@@ -0,0 +1,16 @@
+"use strict";
+// type hasnain = string;
+// let a: hasnain =  "Hasnain";
+// console.log(a);
+let student1 = {
+    name: "Hasnain",
+    age: 20,
+    course: "webdevelopment",
+    isEnrolled: true,
+    addrss: {
+        city: "karachi",
+        country: "Pakistan "
+    }
+};
+console.log(student1);
+console.log(student1.addrss);
