66666bb409b1999bd326df7f9c04459a07624534
https://github.com/Hasnain-KH <webdevhasnain@gmail.com>
2026-06-10 14:57:15 +0500

    add some more practice of Interface

--- Interfaces/app.js
+++ Interfaces/app.js
@@ -1,9 +1,15 @@
 "use strict";
-const userdata = {
-    name: "Hasnain",
-    email: "Hasn@gmail.com"
-};
-function abcd(userdata) {
-    console.log(userdata.email);
+// interface User {
+//     name: string,
+//     email: string,
+//     password : number
+// }
+function abcd(object) {
+    return object;
 }
-abcd(userdata);
+let save = abcd({ name: "Hasnain", password: 2323 });
+let save1 = abcd({ name: "Sufyan", password: 1234 });
+let save2 = abcd({ name: "Aftab", password: 2093 });
+console.log(save);
+console.log(save1);
+console.log(save2);
--- Interfaces/app.ts
+++ Interfaces/app.ts
@@ -1,13 +1,52 @@
-interface User {
-    name: string
-    email: string
-}
-const userdata: User = {
-    name: "Hasnain",
-    email: "Hasn@gmail.com"
-}
-function abcd (userdata: User): void{
-    console.log(userdata.email);
-}
-abcd(userdata);
+// interface User {
+//     name: string,
+//     email: string,
+//     password : number
+// }
+
+// const admin : User ={
+//     name: "Hasnain",
+//     email: "hasnn@gmial.com",
+//     password: 343
+// }
+
+// console.log(admin);
+
+
+
+// interface Admin {
+//     name: string,
+//     location: string,
+//     gender?: string
+// }
+
+// function data (obj: Admin){
+//     return obj;
+// }
+// console.log(data({ name: "Hasnain", location: "mansehra", gender:"male"}));
+// console.log(data({ name: "Ayesha", location: "Lahore"}));
+// console.log(data({ name: "Waseem", location: "Islamabad"}));
+
+
+// Merging two Interfaces //
+// interface Amin{
+//     name : string,
+// }
+
+// interface Amin{
+//     password : number
+// }
+
+// function abcd(object: Amin){
+//     return object
+// }
+
+// let save = abcd({name : "Hasnain", password: 2323});
+// let save1 = abcd({name : "Sufyan", password: 1234});
+// let save2 = abcd({name : "Aftab", password: 2093});
+
+// console.log(save);
+// console.log(save1);
+// console.log(save2);
+
 
