313bf4c447c3d53f77632a375b3ee22e5b845f47
https://github.com/Hasnain-KH <webdevhasnain@gmail.com>
2026-06-11 23:56:14 +0500

    add some practice of Extendein-Interfaces

---
+++ Extending-Interfaces/app.js
@@ -0,0 +1,12 @@
+"use strict";
+// interface User {
+//     name: string,
+//     email: string
+// }
+function userdata(obj) {
+    return obj;
+}
+let showdata = userdata({ name: "Ayan", email: "Ayan@gmail.com" });
+let showdata1 = userdata({ name: "sufyan", email: "sufyan@gmail.com" });
+console.log(showdata);
+console.log(showdata1);
---
+++ Extending-Interfaces/app.ts
@@ -0,0 +1,36 @@
+// interface User {
+//     name: string,
+//     email: string
+// }
+
+// interface Admin extends User {
+//     password : number
+// }
+// function fun(obj: Admin){
+//     return obj;
+// }
+
+// let save = fun({name: "Hasnain", email: "hasnain@gmail.com", password: 3434});
+// let save1 = fun({name: "Hasnain", email: "hasnain@gmail.com"});
+// console.log(save);
+// console.log(save1);
+
+
+// interface User1{
+//     name: string
+// }
+
+// interface User2 extends User1{
+//     email : string
+// }
+
+// function userdata (obj : User2){
+//     return obj;
+// }
+
+// let showdata = userdata({name: "Ayan", email: "Ayan@gmail.com"})
+// let showdata1 = userdata({name: "sufyan", email: "sufyan@gmail.com"})
+// console.log(showdata);
+// console.log(showdata1);
+
+
