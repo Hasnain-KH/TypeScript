b0a26a3e5d074f3c0870ae38a776d8a2294cbec7
https://github.com/Hasnain-KH <webdevhasnain@gmail.com>
2026-07-30 23:58:55 +0500

    add one more practice question of Decorator

--- Decorators/app.ts
+++ Decorators/app.ts
@@ -6,16 +6,28 @@
 
 // class student{}
 
-function logger1 (constructor: Function){
-    console.log("OH, It's running");
+// function logger1 (constructor: Function){
+//     console.log("OH, It's running");
+// }
+// @logger1
+// class person{}
+
+
+// function load(constructor : Function){
+//     console.log("Student class loaded");
+// }
+// @load
+// class student{}
+
+
+function loader(constructor: Function){
+    console.log(constructor.name);
+    
 }
-@logger1
-class person{}
 
+@loader
 
-function load(constructor : Function){
-    console.log("Student class loaded");
-}
-@load
-class student{}
+class admin{}
 
+@loader
+class coreAdmin{}
